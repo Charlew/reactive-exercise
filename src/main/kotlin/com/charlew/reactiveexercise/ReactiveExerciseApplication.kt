4722d23d9e77e11f26d6ed6a4fa5d207703f2e36
Charlew <kwisniewski103@gmail.com>
2020-02-21 19:42:40 +0100

    Initial spring boot project

---
+++ src/main/kotlin/com/charlew/reactiveexercise/ReactiveExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.charlew.reactiveexercise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReactiveExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReactiveExerciseApplication>(*args)
+}
