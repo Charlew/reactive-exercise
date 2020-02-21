c07c0c4d01a2349096b95ca15fecb98947332383
Charlew <kwisniewski103@gmail.com>
2020-02-21 20:38:15 +0100

    Add service

--- src/main/kotlin/com/charlew/reactive_exercise/ReactiveExerciseApplication.kt
+++ src/main/kotlin/com/charlew/reactive_exercise/ReactiveExerciseApplication.kt
@@ -6,8 +6,12 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import reactor.core.Disposable
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.core.publisher.toFlux
+import java.time.Duration
 import java.time.Instant
 import javax.annotation.PostConstruct
 
@@ -16,6 +20,17 @@ class ReactiveExerciseApplication
 
 fun main(args: Array<String>) {
 	runApplication<ReactiveExerciseApplication>(*args)
+}
+
+@Service
+class CoffeeService(private val repository: CoffeeRepository) {
+	fun getAllCoffees(): Flux<Coffee> = repository.findAll()
+
+	fun getCoffeeById(id: String): Mono<Coffee> = repository.findById(id)
+
+	fun getOrdersForCoffeeById(coffeeId: String): Flux<CoffeeOrder> = Flux.interval(Duration.ofSeconds(1))
+		.onBackpressureDrop()
+		.map { CoffeeOrder(coffeeId, Instant.now()) }
 }
 
 @Component
---
+++ src/test/kotlin/com/charlew/reactive_exercise/InternalAPITest.kt
@@ -0,0 +1,23 @@
+package com.charlew.reactive_exercise
+
+import org.junit.Before
+import org.junit.Test
+
+class InternalAPITest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getAllCoffees() {
+    }
+
+    @Test
+    fun getCoffeeById() {
+    }
+
+    @Test
+    fun getOrdersForCoffeeById() {
+    }
+}
